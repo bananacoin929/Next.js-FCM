4ea5c412bd761c64c223256cb4ea6e373a96744f
bananacoin929 <bananacoin929@gmail.com>
2024-11-28 12:22:29 -0500

    fix: update firebase messaging-sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,13 +7,12 @@ importScripts(
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyBen3zHrTdgYrZdK4paON3i3PCSkSv2TWc",
+  authDomain: "dental-2b618.firebaseapp.com",
+  projectId: "dental-2b618",
+  storageBucket: "dental-2b618.firebasestorage.app",
+  messagingSenderId: "517887828515",
+  appId: "1:517887828515:web:d6a968f8b8772e4006c796",
 };
 
 firebase.initializeApp(firebaseConfig);
