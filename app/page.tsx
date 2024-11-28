ba27a8a5b612c823aba3cc445e94979b9da06e0b
bananacoin929 <bananacoin929@gmail.com>
2024-11-28 14:26:05 -0500

    fix: register service worker

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,27 @@
+'use client'
+
+import { useEffect } from "react";
 import NotificationButton from "../components/NotificationButton";
 
+const useServiceWorker = () => {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/firebase-messaging-sw.js")
+        .then((registration) => {
+          console.log("Service Worker registered with scope:", registration.scope);
+        })
+        .catch((error) => {
+          console.error("Service Worker registration failed:", error);
+        });
+    }
+  }, []);
+};
+
+
 export default function HomePage() {
+  useServiceWorker()
+  
   return (
     <main>
       <h1>Firebase Notifications in Next.js</h1>
